926e729c96df37915f24f72528316ec6255fea38
trisrosco <trisrosco@gmail.com>
2023-04-10 19:51:42 +0100

    okay I did it with jest now, I used playwright before and it made a mess

--- main.js
+++ main.js
@@ -3,15 +3,16 @@
 
 You'll be given an array of numbers, and it's up to you to convert those numbers into unicode characters and return them in a string using string interpolation.
 
+For example, if you're given the array [84, 114, 105, 115, 116, 97, 110], you should return the string "Hi, Tristan! Nice to meet you!".
+
 */ 
 
 //👉 Write the function your CodeWarriors will start with below here:
 
 export function convertToUnicode(numbers) {
   //👉 Write your code here:
-    let name = String.fromCharCode(...numbers)
 
-  return (`Hi, ${name}! Nice to meet you!`)
+
 }
 
 
